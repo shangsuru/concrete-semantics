e53d86951523aa74d14ed6e7059a55b46ef60f4e
shangsuru <helm.henry99@gmail.com>
2022-10-11 13:44:18 +0900

    Add examples from Chapter 4

--- Chapter4.thy
+++ Chapter4.thy
@@ -6,12 +6,82 @@ inductive star :: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> 'a \<R
 refl:  "star r x x" |
 step:  "r x y \<Longrightarrow> star r y z \<Longrightarrow> star r x z"
 
+lemma star_trans: "star r x y \<Longrightarrow> star r y z \<Longrightarrow> star r x z"
+  apply(induction rule: star.induct)
+   apply(assumption)
+  apply(metis step)
+done
+
 text{*
 \section*{Chapter 4}
 
 \exercise
 Start from the data type of binary trees defined earlier:
 *}
+
+(* Proof Automation *)
+lemma "\<forall>x. \<exists>y. x=y"
+  by auto
+
+lemma "A \<subseteq> B \<inter> C \<Longrightarrow> A \<subseteq> B \<union> C"
+  by auto
+
+lemma "\<lbrakk>\<forall>xs \<in> A. \<exists>ys. xs = ys @ ys; us \<in> A\<rbrakk> \<Longrightarrow> \<exists>n. length us = n + n"
+  by fastforce
+
+lemma "\<lbrakk> \<forall> x y. T x y \<or> T y x; 
+        \<forall> x y. A x y \<and> A y x \<longrightarrow> x = y;
+        \<forall> x y. T x y \<longrightarrow> A x y \<rbrakk>
+        \<Longrightarrow> \<forall> x y. A x y \<longrightarrow> T x y"
+  by blast
+
+lemma "\<lbrakk> xs @ ys = ys @ xs; length xs = length ys \<rbrakk> \<Longrightarrow> xs = ys"
+  (* sledgehammer *)
+  by (metis append_eq_conv_conj)
+
+lemma "\<lbrakk> (a::nat) \<le> x + b; 2*x < c \<rbrakk> \<Longrightarrow> 2*a + 1 \<le> 2*b + c"
+  by arith
+
+lemma "\<lbrakk> (a::nat) \<le> b; b \<le> c; c \<le> d; d \<le> e \<rbrakk> \<Longrightarrow> a \<le> e"
+  by(blast intro: le_trans)
+
+thm conjI[OF refl[of "a"] refl[of "b"]]
+
+(*Forward Reasoning with Suc_leD: Suc m \<le> n \<Longrightarrow> m \<le> n*)
+lemma "Suc(Suc(Suc a)) \<le> b \<Longrightarrow> a \<le> b"
+  by(blast dest: Suc_leD)
+
+inductive ev :: "nat \<Rightarrow> bool" where
+ev0: "ev 0" |
+evSS: "ev n \<Longrightarrow> ev (Suc (Suc n))"
+
+fun evn :: "nat \<Rightarrow> bool" where
+"evn 0 = True" |
+"evn (Suc 0) = False" |
+"evn (Suc(Suc n)) = evn n"
+
+(* Proof of lemma in forward direction *)
+thm evSS[OF evSS[OF ev0]]
+(* Proof of lemma in backward direction  *)
+lemma "ev(Suc(Suc(Suc(Suc 0))))"
+  apply(rule evSS)
+  apply(rule evSS)
+  apply(rule ev0)
+  done
+
+(* Rule induction *)
+lemma "ev m \<Longrightarrow> evn m"
+  apply(induction rule: ev.induct)
+   apply(simp_all)
+  done
+
+(* evn.induct sets up three subgoals corresponding to the 3 equations of evn *)
+lemma "evn n \<Longrightarrow> ev n"
+  apply(induction n rule: evn.induct)
+    apply(simp_all add: ev0 evSS)
+  done
+
+declare ev.intros[simp,intro]
 
 datatype 'a tree = Tip | Node "'a tree" 'a "'a tree"
 
