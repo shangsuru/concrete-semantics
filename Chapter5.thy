7bfd4ea786ac08e121c8cd7cacbdd4dbfe75de99
shangsuru <helm.henry99@gmail.com>
2022-10-13 12:37:45 +0900

    Proof 5.1 and 5.2

--- Chapter5.thy
+++ Chapter5.thy
@@ -2,6 +2,13 @@ theory Chapter5
 imports Main
 begin
 
+lemma "\<not> surj(f :: 'a \<Rightarrow> 'a set)"
+proof
+  assume 0: "surj f"
+  hence "\<exists>a. {x. x \<notin> f x} = f a" by(auto simp: surj_def)
+  thus "False" by blast
+qed
+
 inductive star :: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> bool"  for r where
 refl:  "star r x x" |
 step:  "r x y \<Longrightarrow> star r y z \<Longrightarrow> star r x z"
@@ -20,7 +27,19 @@ lemma assumes T: "\<forall>x y. T x y \<or> T y x"
   and A: "\<forall>x y. A x y \<and> A y x \<longrightarrow> x = y"
   and TA: "\<forall>x y. T x y \<longrightarrow> A x y" and "A x y"
   shows "T x y"
-(* your definition/proof here *)
+proof -
+  have "T x y \<or> T y x" using T by blast
+  then show "T x y"
+  proof
+    assume "T x y"
+    then show "T x y" by simp
+  next
+    assume "T y x"
+    hence "A y x" using TA by blast
+    hence "x = y" using assms(4) A by blast
+    then show "T x y" using \<open>T y x\<close> by auto
+  qed
+qed
 
 text{*
 Each step should use at most one of the assumptions @{text T}, @{text A}
@@ -32,7 +51,26 @@ Give a readable, structured proof of the following lemma:
 *}
 lemma "(\<exists>ys zs. xs = ys @ zs \<and> length ys = length zs)
       \<or> (\<exists>ys zs. xs = ys @ zs \<and> length ys = length zs + 1)"
-(* your definition/proof here *)
+proof cases
+  let ?len_ys = "length xs div 2"
+  assume 1: "length xs mod 2 = 0" 
+  then obtain ys where 2: "ys = take ?len_ys xs" by blast
+  then obtain zs where "zs = drop ?len_ys xs" by blast
+  hence "xs = ys @ zs \<and> length ys = length zs" 
+    using 1 2
+    by fastforce
+  thus ?thesis by blast
+next
+  let ?len_ys = "length xs div 2 + 1"
+  assume 1: "length xs mod 2 \<noteq> 0"
+  then obtain ys where 2: "ys = take ?len_ys xs" by blast
+  then obtain zs where "zs = drop ?len_ys xs" by blast
+  hence "xs = ys @ zs \<and> length ys = length zs + 1" 
+    using 1 2
+    by (smt (verit, ccfv_SIG) add.assoc add.commute add.right_neutral add_diff_cancel_right' add_self_div_2 append_take_drop_id div_add1_eq drop_drop length_append length_drop not_mod_2_eq_0_eq_1)
+  thus ?thesis by blast
+qed
+    
 
 text{*
 Hint: There are predefined functions @{const take} and {const drop} of type
